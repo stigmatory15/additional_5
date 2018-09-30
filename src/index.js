791edfba9ab1bf10470c3f3f68e9555b56a61d82
stigmatory15 <repka_dmitriy@mail.ru>
2018-09-30 18:38:53 +0300

    task 6 60/40

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ module.exports = function check(str, bracketsConfig) {
     for (let i = 1; i < str.length; i++) {
         for (let j = 0; j < bracketsConfig.length; j++) {
             for (let k = i - 1; k >= 0; k--) {
-                if (str[i] == bracketsConfig[j][1] && array[k] == bracketsConfig[j][0]) {
+                if (str[i] == bracketsConfig[j][0]) {
+                    if(bracketsConfig[j][0] == bracketsConfig[j][1] && str[i] == array[k]){
+                        array.pop();
+                    }
+                    array.push(str[i])
+                }
+
+                if (str[i] == bracketsConfig[j][1] && array[k] == bracketsConfig[j][0]){
                     array.pop();
                 }
+
             }
-            if (str[i] == bracketsConfig[j][0])
-                array.push(str[i])
         }
     }
     if(array.length == 0) return true;
